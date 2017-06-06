8fc55e81dba9171bd8b5085a4d8ec7d4c51abdb6
Hobgoblin101 <aj@bilby.net>
2017-06-06 18:31:25 +1000

    Added Location Finding
    Added search for current location from memory function

--- bin/robot.js
+++ bin/robot.js
@@ -1,18 +1,88 @@
 class Robot{
   constructor(){
+    //For simulation purposes only!!!
     this.loc = new Vector2();
     this.rotation = new Rotation();
 
+    this.offset = new Vector2();
+
+    //Lay of the land knowledge
+    this.hits = new NArray2D();
+    this.memory = new NArray2D();
+
+    //Recorded stepperMotor position
     this.stepperMotor = {
       steps: 0,
-      maxSteps: 15,
+      maxSteps: 360,
       rotation: new Rotation(0)
     };
 
     this.recentProbs = [];
   }
 
+  calculateOffset(){
+    var self = this;
+    var best = {
+      x: 0,
+      y: 0,
+      p: 0,
+      n: 1 //Number of equal bests
+    };
+
+    SpiralLoop(9, function(x, y){
+      var p = self.matchGridOffset(
+        x + (self.offset.x/gridSize),
+        y + (self.offset.y/gridSize)
+      );
+      if (p > best.p){
+        best.x = x;
+        best.y = y;
+        best.p = p;
+        best.n = 1;
+      }else if (best.p !== 0 && best.p === p){
+        //If equal best, add to the average
+        best.x = best.x*best.n + x *(best.n+1);
+        best.y = best.y*best.n + y *(best.n+1);
+        best.n += 1;
+      }
+    });
+
+    console.log(best);
+
+    if (best.p >= 0.25){
+      this.offset.x = best.x * gridSize;
+      this.offset.y = best.y * gridSize;
+
+      return best.p;
+    }
+
+    return best.p;
+  }
+
+  matchGridOffset(offx, offy){
+    var total = 0;
+    var correct = 0;
+
+    var self = this;
+
+    self.hits.forEach(function(value, x, y){
+      total += 1;
+      var t = self.memory.get(x + offx, y + offy);
+      if (t !== undefined){
+        if (t == value){
+          correct += 1;
+        }else{
+          // correct -= 1;
+        }
+      }
+    });
+
+    return correct / total;
+  }
+
   update(probDist){
+    var self = this;
+
     //Add distance
     var point = new Vector2(0, probDist);
     point.rotation = this.stepperMotor.rotation;
@@ -23,11 +93,22 @@ class Robot{
     //Rotate Prob
     this.stepperMotor.steps = (this.stepperMotor.steps+1) % this.stepperMotor.maxSteps;
     this.stepperMotor.rotation.degrees = 360 * (this.stepperMotor.steps / this.stepperMotor.maxSteps);
+
+    if (this.stepperMotor.steps === 0){
+      this.calculateOffset();
+
+      this.hits.forEach(function(value, x, y){
+        self.hits.set(x, y, false);
+        // value = false;
+      });
+      for (let item of this.recentProbs){
+        this.hits.set(item.x/gridSize, item.y/gridSize, true);
+      }
+    }
   }
 
   draw(){
     push();
-    fill(150);
 
     translate(this.loc.x, this.loc.y);
     rotate(this.rotation.radians);
@@ -37,21 +118,50 @@ class Robot{
         continue;
       }
 
-      stroke(255-i, 0,0);
+      stroke(255*(1-i/this.recentProbs.length), 0,0);
 
       line(0,0, this.recentProbs[i].x, this.recentProbs[i].y);
       noStroke();
-      // ellipse(this.recentProbs[i].x, this.recentProbs[i].y, 10, 10);
     }
 
+    //Draw known probs
+    fill(0, 255, 0, 120);
+    this.hits.forEach(function(value, x, y){
+      if (value){
+        rect(
+          x*gridSize - gridSize/2,
+          y*gridSize - gridSize/2,
+          gridSize,
+          gridSize
+        );
+      }
+    });
+
+
     //Draw robot
+    fill(150);
     ellipse(0, 0, 20, 20);
     stroke(100);
     line(0,0,0,10);
 
     pop();
 
-    // this.move(0.01);
+    //Draw robot estimated location
+    fill(0, 255, 0, 100);
+    ellipse(this.offset.x, this.offset.y, 10, 10);
+
+    //Draw Memory
+    fill(0, 0, 255, 50);
+    this.memory.forEach(function(value, x, y){
+      if (value){
+        rect(
+          x*gridSize - gridSize/2,
+          y*gridSize - gridSize/2,
+          gridSize,
+          gridSize
+        );
+      }
+    });
   }
 
   move(dist){
@@ -66,9 +176,15 @@ class Robot{
 
 var robot = new Robot();
 
+
+
+
+
 setTimeout(function(){
   //speedOfSound 340.29   m/s
   //             34029000 cm/ms
+
+  robot.memory = world;
 
   function UltraSonicLoop(){
     var dist = DistanceProb(robot.loc, robot.stepperMotor.rotation);
@@ -80,4 +196,4 @@ setTimeout(function(){
   }
 
   UltraSonicLoop();
-}, 1000);
+}, 1);
---
+++ bin/spiralLoop.js
@@ -0,0 +1,21 @@
+function SpiralLoop(searchSize, callback){
+  var range = searchSize ** 2;
+
+  var x = 0;
+  var y = 0;
+  var dx = 0;
+  var dy = -1;
+
+  for (let i=0; i<range; i++){
+    if ((-searchSize/2 < x && x < searchSize/2) && (-searchSize/2 < y && y < searchSize/2)){
+      callback(x, y);
+    }
+    if (x === y || (x < 0 && x == -y) || (x > 0 && x == 1-y)){
+      var t = dx; //Temp
+      dx = -dy;
+      dy = t;
+    }
+    x += dx;
+    y += dy;
+  }
+}
